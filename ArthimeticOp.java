b13157becda83114c02a8aeba35709659fa01129
sreejam12 <130955789+sreejam12@users.noreply.github.com>
2024-05-31 17:16:41 +0530

    Add files via upload

---
+++ ArthimeticOp.java
@@ -0,0 +1,23 @@
+package operators;
+
+public class ArthimeticOp {
+
+	public static void main(String[] args) {
+		
+      int sum = 10+5;
+      int difference = 10-5;
+      int product = 10*5;
+      int quotient1 = 10/3;
+      double quotient2 = 10.0/3;
+      int remainder = 10%3;
+      System.out.println("sum:" +sum);
+      System.out.println("difference:" +difference);
+      System.out.println("Product:" +product);
+      System.out.println("quotient1:" +quotient1);
+      System.out.println("quotient2:" +quotient2);
+      System.out.println("remainder:" +remainder);
+    	  
+
+	}
+
+}
