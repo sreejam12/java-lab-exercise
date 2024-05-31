4456bd73cb413310b9f3bdf11cf53a9a8bbd5603
sreejam12 <130955789+sreejam12@users.noreply.github.com>
2024-05-31 17:17:22 +0530

    Add files via upload

---
+++ ArithOperEx.java
@@ -0,0 +1,17 @@
+package operators;
+
+public class ArithOperEx {
+	public static void main(String[] args) {
+	int a=10,b=3;
+	a+=b;
+	System.out.println(a);
+	a-=b;
+	System.out.println(a);
+	a*=b;
+	System.out.println(a);
+	a=a/b;
+	System.out.println(a);
+	a=a%b;
+	System.out.println(a);
+}
+}
