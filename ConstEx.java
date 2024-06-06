a77668ce3798e4c77d6d888ed19ca59618f931b3
sreejam12 <130955789+sreejam12@users.noreply.github.com>
2024-06-06 14:10:59 +0530

    Add files via upload

---
+++ ConstEx.java
@@ -0,0 +1,30 @@
+package june;
+
+public class ConstEx {
+   public ConstEx() {
+	   System.out.println("Constructor Example"); 
+	   }
+   public ConstEx(int a) {
+	   a++;
+	   System.out.println(a);
+   }
+   void call(String msg) {
+	   System.out.println(msg);
+   }
+    int add(int a,int b) {
+	   return a+b;
+   }
+    char show() {
+    	return 'a';
+    }
+	public static void main(String[] args) {
+		ConstEx on = new ConstEx();
+		ConstEx on2 = new ConstEx(3);
+		on2.call("Welcome");
+		System.out.println(on.add(10,11));
+		 char ch = on.show();
+		 System.out.println(ch);
+
+	}
+
+}
