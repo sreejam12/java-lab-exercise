b66713a5488d52a6fe691db09194386cea6251de
sreejam12 <130955789+sreejam12@users.noreply.github.com>
2024-06-06 14:11:21 +0530

    Add files via upload

---
+++ LargestOfThree.java
@@ -0,0 +1,28 @@
+package june;
+import java.util.Scanner;
+public class LargestOfThree {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter first number");
+        int num1 = sc.nextInt();
+        
+        System.out.println("Enter second number");
+        int num2 = sc.nextInt();
+        
+        System.out.println("Enter third number");
+        int num3 = sc.nextInt();
+        
+        if(num1 >= num2 && num1 >= num3) {
+        	System.out.println(num1 + " is largest value");
+        }
+        else if(num2 >=  num3 && num2 >= num1) {
+        	System.out.println(num2 + "is largest value");
+        }
+        else {
+        	System.out.println(num3 + "is largest value");
+        }
+	}
+
+}
